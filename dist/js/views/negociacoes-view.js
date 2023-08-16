91449bd83ffeeffdc6d2f35d8c4d4c766cd55697
Bruno Gonçalves <b.goncalves484@gmail.com>
2023-08-16 07:43:01 -0300

    feature: criando primeira view (tabela)

---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,23 @@
+export class NegociacoesView {
+    constructor(selector) {
+        this.elemento = document.querySelector(selector);
+    }
+    template() {
+        return `
+        <table class="table table-hove table-bordered">
+            <thead>
+                <tr>
+                  <th>DATA</th>
+                  <th>QUANTIDADE</th>
+                  <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+        `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
+    }
+}

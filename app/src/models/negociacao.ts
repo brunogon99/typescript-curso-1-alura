1995e23a9bcd0a59f707e5a66999612339f74c47
Bruno Gonçalves <b.goncalves484@gmail.com>
2023-09-19 22:08:22 -0300

    feat: estendendo interfaces

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,7 +1,6 @@
-import { Comparavel } from "../interfaces/comparavel.js";
-import { Imprimivel } from "../utils/imprimivel.js";
+import { Modelo } from "../interfaces/modelo.js";
 
-export class Negociacao implements Imprimivel, Comparavel<Negociacao> {
+export class Negociacao implements Modelo<Negociacao> {
   /* 
   está sendo declarado diretamente no construtor, este é um recurso do typescript
   para reduzi a quantidade de código escrito.
--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,8 +1,7 @@
-import { Comparavel } from "../interfaces/comparavel.js";
-import { Imprimivel } from "../utils/imprimivel.js";
+import { Modelo } from "../interfaces/modelo.js";
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
+export class Negociacoes implements Modelo<Negociacoes> {
     private negociacoes: Negociacao[] = []; // é o mesmo que Array<Negociacao>
 
     public adiciona(negociacao: Negociacao) {
